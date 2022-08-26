d15907ff6f92a57e063ff2787e2db3cbdc13c115
Pierce Thompson <insprill@gmail.com>
2022-08-25 23:48:47 -0400

    Switch to spigot nms dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,13 @@ group = "net.insprill"
 version = "0.1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.19.2-R0.1-SNAPSHOT")
 }
 
 tasks {
