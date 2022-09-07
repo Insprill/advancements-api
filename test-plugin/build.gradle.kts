b960b65046b1936039f34e5f072ff31744420776
Pierce Thompson <insprill@gmail.com>
2022-09-07 00:11:42 -0400

    Add a test plugin

---
+++ test-plugin/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.rikonardo.papermake") version "1.0.4"
+}
+
+repositories {
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    implementation(project(":"))
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        filesMatching("plugin.yml") {
+            expand("version" to version)
+        }
+    }
+
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
